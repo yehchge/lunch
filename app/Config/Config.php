667080e4158b46cd3d617d82a291d4a84503dc98
bill.yeh <bill.yeh@iwant-in.net>
2025-04-11 18:28:08 +0800

    刪除共用參數

--- app/Config/Config.php
+++ app/Config/Config.php
@@ -8,7 +8,10 @@ declare(strict_types=1); // 嚴格類型
 
 defined('PATH_ROOT')|| define('PATH_ROOT', realpath(dirname(__FILE__) . '/../..'));
 
-require PATH_ROOT."/vendor/autoload.php";
+// require PATH_ROOT."/vendor/autoload.php";
+
+require PATH_ROOT.'/app/System/DotEnv.php';
+
 use Lunch\System\DotEnv;
 (new DotEnv(PATH_ROOT . '/.env'))->load();
 
@@ -18,8 +21,7 @@ use Lunch\System\DotEnv;
 // echo getenv('LUNCH_ENV');echo "<br>";
 // echo getenv("DATABASE_HOST");exit;
 
-include_once PATH_ROOT."/gphplib/DebugConsole.php";
-
+require PATH_ROOT.'/app/System/DebugConsole.php';
 require PATH_ROOT.'/app/System/Database.php';
 require PATH_ROOT.'/app/Repository/UserRepository.php';
 require PATH_ROOT.'/app/Auth/Auth.php';
