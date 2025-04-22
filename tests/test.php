63f9fdff400593302f64e47998f16fd683390b8a
bill.yeh <bill.yeh@iwant-in.net>
2025-04-22 10:36:59 +0800

    移除未使用的程式

--- tests/test.php
+++ tests/test.php
@@ -1,18 +1,19 @@
 <?php
-    $dbms='mysql'; //資料庫類型
-    $host='172.16.1.230';      //資料庫主機名稱
-    $dbName='test_bill';    //資料庫名稱
-    $user='robot';      //連線帳號
-    $pass='robot';      //連線密碼
-    $dsn="$dbms:host=$host;dbname=$dbName";
-    try
-    {
-        $dbh = new PDO($dsn, $user, $pass);
-        echo "資料庫連線成功<br/>";
-        $dbh = null;
-    }
-    catch (PDOException $e) 
-    {
-        die("Error!: " . $e->getMessage() . "<br/>");
-    }
-    $db = new PDO($dsn, $user, $pass, array(PDO::ATTR_PERSISTENT => true));
+
+$dbms='mysql'; //資料庫類型
+$host='172.16.1.230';      //資料庫主機名稱
+$dbName='test_bill';    //資料庫名稱
+$user='robot';      //連線帳號
+$pass='robot';      //連線密碼
+$dsn="$dbms:host=$host;dbname=$dbName";
+try
+{
+    $dbh = new PDO($dsn, $user, $pass);
+    echo "資料庫連線成功<br/>";
+    $dbh = null;
+}
+catch (PDOException $e) 
+{
+    die("Error!: " . $e->getMessage() . "<br/>");
+}
+$db = new PDO($dsn, $user, $pass, array(PDO::ATTR_PERSISTENT => true));
